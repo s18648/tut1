a71e61a4a31443ee6f559dbce63124da1d539fee
Weronika Nurzyńska <s18648@pjwstk.edu.pl>
2020-03-05 16:56:15 +0100

    initial_Commit

---
+++ Tutorial1_Solution/Tutorial1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Tutorial1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var websiteur1 = args[0];
+            //Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteur1);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(htmlContent);
+
+                foreach (var emailAddress in matches)
+                {
+                    Console.WriteLine(emailAddress.ToString());
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
